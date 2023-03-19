efc18216bc98dcde5b08a5b78010060985600772
Mihir Madhira <right2mihir@gmail.com>
2023-03-19 15:21:12 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express'); 
 const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
 app.use(express.json());
 
-app.listen(port, () => console.log('Listening on Port 3000'));
+
+
+
+
+app.listen(80, () => console.log('Listening on Port 80'));
