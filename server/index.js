4aa9839ff70d5acf6ec9bd2e6e91263bf7264bf0
Mihir Madhira <right2mihir@gmail.com>
2023-03-19 00:48:07 -0400

    index.js file

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => console.log('Listening on Port 3000'));
