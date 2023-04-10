bbf7162bf72e839e47134f949f26bfc6e7a24e87
SameerK75 <sameerkmun@gmail.com>
2023-04-10 00:42:03 -0400

    display table data with detail view button

--- client/src/components/TableView.js
+++ client/src/components/TableView.js
@@ -1,53 +1,68 @@
-import React from 'react'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
+import React, { useEffect, useState } from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const TableView = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align='right'>Owner</TableCell>
-              <TableCell align='right'>
-                Visibility&nbsp;(public, private)
-              </TableCell>
-              <TableCell align='right'>
-                View Type&nbsp;(table, detail)
-              </TableCell>
-              <TableCell align='right'>Roles&nbsp;(admin, user)</TableCell>
-              <TableCell align='right'>Allowed Actions&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.view.map((info) => (
-              <TableRow
-                key={info.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {info.name}
-                </TableCell>
-                <TableCell align='right'>{info.owner}</TableCell>
-                <TableCell align='right'>{info.visibility}</TableCell>
-                <TableCell align='right'>{info.view_type}</TableCell>
-                <TableCell align='right'>{info.roles}</TableCell>
-                <TableCell align='right'>{info.allowed_actions}</TableCell>
+const columns = ["First Name", "Last Name", "ID", "HW1", "HW2"];
+const rows = [["Sameer", "Khan", "1", "90", "95"], ["Moh", "How", "2", "100", "99"], ["Sid", "Sham", "3", "95", "96"], 
+["Mihir", "Mad", "4", "100", "100"] ];
+
+
+export default function TableView() {
+
+    const [colNames, setColNames] = useState([]);
+    //const [rows, setRows] = useState([]);
+
+    //get data by rows
+    //first row is column headings
+    //can shift array to remove column headings array and then array.map each column
+
+    useEffect(() => {
+        //get the view data
+        //get table data
+    }, []);
+
+
+    return(
+        <div>
+            <Box>
+                <Typography sx = {{borderBottom: "2px solid black", width: "100%"}} variant = "h2">Table Title</Typography>
+            </Box>
+        <TableContainer sx = {{maxWidth: "100%"}} component={Paper}>
+          <Table sx={{ }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                    <TableCell key = {column} align = "center" key = {column}>{column}</TableCell>
+                ))}
+                <TableCell></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  )
-}
+            </TableHead>
+            <TableBody>
+                {rows.map((row) => (
+                    <TableRow >
+                        {row.map((value) => (
+                            <TableCell align = "center">{value}</TableCell>
+                        ))}
+                        <TableCell sx = {{width: "150px"}} align = "center">
+                            <Button variant = "contained">Detail View</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>    
+    )
 
-export default TableView
+
+    
+
+}
